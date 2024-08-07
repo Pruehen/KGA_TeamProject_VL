e44027e4c39fd1693f39f7e0624be1dc07755423
변석진 <bsj2234@naver.com>
2024-08-07 16:38:30 +0900

    [fix] 아이템의 모델과 리지드바디 분리로 인한 코드 수정

---
+++ Assets/Scripts/BSJ/Item/TrashItem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashItem : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private Collider[] _collider;
+
+    private void Awake()
+    {
+        _collider = transform.GetChild(0).GetComponentsInChildren<Collider>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    public void DisablePhysics()
+    {
+        _rigidbody.isKinematic = true;
+        foreach (var collider in _collider)
+        {
+            collider.isTrigger = true;
+        }
+    }
+    public void EnablePhysics()
+    {
+        _rigidbody.isKinematic = false; 
+        foreach (var collider in _collider)
+        {
+            collider.isTrigger = false;
+        }
+    }
+}
