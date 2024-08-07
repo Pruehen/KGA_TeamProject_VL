ba0e06eebb2d714f8e743b8e8c17f1b120c39db9
jihe05 <163379277+jihe05@users.noreply.github.com>
2024-08-07 15:09:39 +0900

    [feat] PlayerinstanteState 스크립트 추가 (스테미나 조절)

---
+++ Assets/Scripts/CJH/PlayerInstanteState.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public class PlayerInstanteState : MonoBehaviour
+{
+    float hp;
+    float stamina;
+
+    [SerializeField]
+    private float maxHp;
+
+    [SerializeField]
+    private float MaxStamina;
+
+    [SerializeField]
+    private float staminaRecoverySpeed;
+
+    private void Start()
+    {
+        stamina = MaxStamina;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+           
+            StaminaConsumption(10);
+        }
+
+        StaminaAutoRecovery();
+    }
+
+    //스태미나 소모 
+    public void StaminaConsumption(float power)
+    {
+        if (stamina > power)
+            stamina -= power;
+        else
+            return;
+
+        Debug.Log("stamina : " + stamina);
+
+    }
+
+    //스태미나 자동 회복
+    public void StaminaAutoRecovery()
+    {
+        if (stamina < MaxStamina)
+        {
+            stamina += staminaRecoverySpeed * Time.deltaTime;
+            Debug.Log("stamina : " + stamina);
+        }
+        else if(stamina > MaxStamina)
+        {
+            stamina = MaxStamina;
+            return;
+        }
+
+    
+    
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
