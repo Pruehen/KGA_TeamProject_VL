5c6c13eacf030ab2e9ac58922e960e7a64a6c161
권지호 <imperiaiiist@naver.com>
2024-08-07 15:51:40 +0900

    [Fix] 캠 위치 조정용 레이의 최대 거리 조정

--- Assets/Scripts/CJH/PlayerCameraMove.cs
+++ Assets/Scripts/CJH/PlayerCameraMove.cs
@@ -28,7 +28,7 @@ public class PlayerCameraMove : SceneSingleton<PlayerCameraMove>
         Vector3 rayDir = transform.position - player.position;
 
         //플레이어에서 부터의 Ray발사
-        if (Physics.Raycast(player.position, rayDir, out RaycastHit hit, float.MaxValue, camraCollition))
+        if (Physics.Raycast(player.position, rayDir, out RaycastHit hit, camRange, camraCollition))
         {
             //맞은 부위보다 더 안쪽으로 
             Vector3 point = hit.point - rayDir.normalized;
