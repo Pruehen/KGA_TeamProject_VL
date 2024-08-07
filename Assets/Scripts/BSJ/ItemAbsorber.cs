a78b808528524a8ed985bc9bebc159b120272fba
변석진 <bsj2234@naver.com>
2024-08-07 12:49:44 +0900

    [update] 주석 ㅁ 추가

--- Assets/Scripts/BSJ/ItemAbsorber.cs
+++ Assets/Scripts/BSJ/ItemAbsorber.cs
@@ -20,6 +20,7 @@ public class ItemAbsorber : MonoBehaviour
     [SerializeField] private float AbsolsionSpeed = 30f;
     [SerializeField] private AnimationCurve RadiusExpandCurve;
     //¤±
+    //a
 
     //Ä³½Ã
     private CapsuleCollider _collider;
