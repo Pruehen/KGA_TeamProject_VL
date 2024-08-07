bec9fbfc1a675d68fc6c0d2edbed76ca8ae8cd27
김민석 <ogu590626@gmail.com>
2024-08-07 12:19:39 +0900

    [update] player

--- Assets/Scripts/KMS/PlayerMove.cs
+++ Assets/Scripts/KMS/PlayerMove.cs
@@ -1,15 +1,16 @@
 using System.Threading;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
     private Vector2 moveInput = Vector2.zero;
     private Vector2 moveVector = Vector2.zero;
-    public float moveSpeed = 2f;
-    public float speed = 2f;
-    public float rotSpeed = 2f;
-    
+    public float moveSpeed = 5f;
+    public float speed = 5f;
+    public float rotSpeed = 40f;
+
     private CharacterController controller;
     public Animator animator;
     public Camera Cam;
@@ -17,7 +18,6 @@ public class PlayerMove : MonoBehaviour
     public Transform atkPoint;
     public GameObject Item;
 
-    public float Rotate;
     private void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -32,16 +32,23 @@ public class PlayerMove : MonoBehaviour
     {
         moveInput = inputValue.Get<Vector2>();
         Debug.Log(moveInput);
-        RotateCharacter();
-        //MoveOrder();
     }
 
     void Atk()
     {
-        RotateCharacter();
-         GameObject items = Instantiate(Item,atkPoint.position,transform.rotation);
-    }
 
+        GameObject items = Instantiate(Item, atkPoint.position, transform.rotation);
+        RotateCharacter();
+    }
+    void RotateCharacter()
+    {
+        if (moveInput != Vector2.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveInput, Vector3.up);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotSpeed * Time.deltaTime);
+        }
+
+    }
     void OnAtk(InputValue value)
     {
         // 공격 로직
@@ -69,25 +76,23 @@ public class PlayerMove : MonoBehaviour
 
     void OnRotate(InputValue value)
     {
-        //인풋시스템활용 카메라 회전
-        //Rotate = value.Get<Vector2>().x;
-        //Debug.Log(value.Get<Vector2>());
-        //Cam.transform.Rotate(Vector3.up * Rotate);
         Debug.Log("Rotate triggered");
-    }
-
-    private void RotateCharacter()
-    {
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, Cam.transform.eulerAngles.y, 0f), rotSpeed );
     }
 
     private void MoveOrder()
     {
         moveVector = Vector2.Lerp(moveVector, moveInput * moveSpeed * speed, Time.deltaTime * 5);
-        Vector3 moveVector3 = transform.right * moveVector.x + transform.forward * moveVector.y;
-        Vector3 moveVector4 = transform.right * moveVector.x + Cam.transform.forward * moveVector.y;
-        //transform.rotation = moveVector4;
-        controller.Move(moveVector4 * Time.deltaTime);
+        Vector3 moveDirection = new Vector3(moveVector.x, 0, moveVector.y);
+
+        // 캐릭터를 이동 방향으로 회전
+        if (moveDirection != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotSpeed * Time.deltaTime);
+        }
+
+        Vector3 moveVector3 = Cam.transform.right * moveVector.x + Cam.transform.forward * moveVector.y;
+        controller.Move(moveVector3 * Time.deltaTime);
 
         // 애니메이션 설정
         // animator.SetFloat("XSpeed", moveVector.x);
