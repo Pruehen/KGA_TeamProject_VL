9ff3718329004fd261b893ffce966944b0f4bdd7
최지혜 <a46678190@gmail.com>
2024-08-09 11:25:48 +0900

    [feat] 카메라 시네머신으로 변경 및 프리팹 수정

--- Assets/Scripts/CJH/PlayerCameraMove.cs
+++ Assets/Scripts/CJH/PlayerCameraMove.cs
@@ -29,22 +29,22 @@ public class PlayerCameraMove : SceneSingleton<PlayerCameraMove>
 
         camAxis.Rotate(Vector3.up * mouseX);
 
-        //플레이어부터 카메라까지의 방향
-       Vector3 rayDir = transform.position - camAxis.position;
+       // //플레이어부터 카메라까지의 방향
+       //Vector3 rayDir = transform.position - camAxis.position;
 
 
-        Debug.DrawRay(camAxis.position, rayDir.normalized);
+       // Debug.DrawRay(camAxis.position, rayDir.normalized);
 
-        if (Physics.Raycast(camAxis.position, rayDir, out RaycastHit hit, camRange, camraCollition))
-        {
-            Vector3 point = hit.point - rayDir.normalized;
-            transform.position = point;
-            Debug.Log(rayDir);
-        }
-        else
-        {
-            transform.localPosition = Vector3.Lerp(transform.localPosition, startLocalPosition, Time.deltaTime * 10);
-        }
+       // if (Physics.Raycast(camAxis.position, rayDir, out RaycastHit hit, camRange, camraCollition))
+       // {
+       //     Vector3 point = hit.point - rayDir.normalized;
+       //     transform.position = point;
+       //     Debug.Log(rayDir);
+       // }
+       // else
+       // {
+       //     transform.localPosition = Vector3.Lerp(transform.localPosition, startLocalPosition, Time.deltaTime * 10);
+       // }
     }
       
     public Transform CamAxisTransform()
