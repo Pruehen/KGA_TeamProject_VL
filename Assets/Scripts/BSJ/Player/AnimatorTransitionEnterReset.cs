a45a41f4e0ae3b0892237d6f407e6c16dc94b6fb
김민석 <ogu590626@gmail.com>
2024-08-13 14:41:57 +0900

    [update] 스킬애니메이터 수정중

--- Assets/Scripts/BSJ/Player/AnimatorTransitionEnterReset.cs
+++ Assets/Scripts/BSJ/Player/AnimatorTransitionEnterReset.cs
@@ -6,6 +6,7 @@ public class AnimatorTransitionEnterReset : StateMachineBehaviour
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.ResetTrigger(triggerName);
+     
     }
+    
 }
