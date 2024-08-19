1ac3c2f624933f0cd3bad2925071b13c395d209a
김민석 <ogu590626@gmail.com>
2024-08-19 17:15:57 +0900

    [update] 스킬데이터,스킬수정,에니메이터수정

---
+++ Assets/Scripts/KMS/AnimatorEnterSkill.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorEnterSkill : StateMachineBehaviour
+{
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       
+
+    }
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+}
