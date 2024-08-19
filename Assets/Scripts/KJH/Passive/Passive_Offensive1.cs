00224afeaea066d15f4d3622f823d378aa6edf9b
권지호 <imperiaiiist@naver.com>
2024-08-19 14:54:29 +0900

    [Feat] 패시브1 클래스 구현

--- Assets/Scripts/KJH/Passive/Passive_Offensive1.cs
+++ Assets/Scripts/KJH/Passive/Passive_Offensive1.cs
@@ -6,13 +6,15 @@ public class Passive_Offensive1 : Passive
     {
         base._passiveData = JsonDataManager.GetPassive(PassiveID.Offensive1);
     }
-    public override void Active()
+    public override void Active()//현재 체력이 80% 이상일 때, 일반&특수 공격력이 15%만큼 증가
     {
-        
+        base._state.AttackPowerMulti += base._passiveData.VelueList[1];
+        base._state.SkillPowerMulti += base._passiveData.VelueList[2];
     }
 
     public override void DeActive()
     {
-        
+        base._state.AttackPowerMulti -= base._passiveData.VelueList[1];
+        base._state.SkillPowerMulti -= base._passiveData.VelueList[2];
     }
 }
