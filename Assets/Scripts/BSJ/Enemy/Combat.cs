e61e61ebc894ddb6958eb8d1757ff9141ddb03b7
변석진 <bsj2234@naver.com>
2024-08-22 14:01:10 +0900

    [update] 무적 추가중...

--- Assets/Scripts/BSJ/Enemy/Combat.cs
+++ Assets/Scripts/BSJ/Enemy/Combat.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [Serializable]
 public class Combat
 {
+    GameObject _owner;
+
     [SerializeField] private float _maxHp = 100f;
 
     [SerializeField] private float _hp = 100f;
@@ -12,6 +14,7 @@ public class Combat
     [SerializeField] private bool _dead = false;
 
     [SerializeField] private float _invincibleTimeOnHit = .1f;
+
     [SerializeField] private float _prevHitTime = 0f;
 
     public Func<bool> AdditionalDamageableCheck { get; set; }
@@ -24,13 +27,76 @@ public class Combat
     public Action OnKillEnemy;
     public Action<Combat, float> OnAttack;
 
+    private bool _isInvincible;
+    private float _invincibleTimer = .1f;
+    public Action OnInvincible;
+    public Action OnReleaseInvincible;
 
-    public void Init(float maxHp)
+    private bool _isSuperArmor;
+    private float _superArmorTimer = .1f;
+    public Action OnSuperArmor;
+    public Action OnReleaseSuperArmor;
+
+    private bool _isEvade;
+    private float _evadeTimer = .1f;
+    public Action OnEvade;
+    public Action OnReleaseEvade;
+
+    public Action OnKnockBack;
+
+    public void Init(GameObject owner, float maxHp)
     {
+        _owner = owner;
+        _defaultLayer = owner.layer;
         _maxHp = maxHp;
         _hp = maxHp;
         ResetDead();
     }
+
+    public void DoUpdate(float deltaTime)
+    {
+        if(_evadeTimer > 0f)
+        {
+            _evadeTimer -= deltaTime;
+        }
+        else
+        {
+            if (_isEvade)
+            {
+                _owner.layer = _defaultLayer;
+                OnReleaseEvade?.Invoke();
+                _isEvade = false;
+            }
+        }
+
+
+        if (_invincibleTimer > 0f)
+        {
+            _invincibleTimer -= deltaTime;
+        }
+        else
+        {
+            if(_isInvincible)
+            {
+                OnReleaseInvincible?.Invoke();
+                _isInvincible = false;
+            }
+        }
+
+        if (_superArmorTimer > 0f)
+        {
+            _superArmorTimer -= deltaTime;
+        }
+        else
+        {
+            if(_isSuperArmor)
+            {
+                OnReleaseSuperArmor?.Invoke();
+                _isSuperArmor = false;
+            }
+        }
+    }
+
     public float GetHp() { return _hp; }
     public float GetMaxHp()
     {
@@ -40,7 +106,7 @@ public class Combat
 
     private bool IsDamageable()
     {
-        if (Time.time < _prevHitTime + _invincibleTimeOnHit)
+        if (_isInvincible)
         {
             return false;
         }
@@ -59,11 +125,17 @@ public class Combat
         }
         return true;
     }
-    public bool Damaged(float damage)
+    public bool Damaged(float damage, Transform attacker = null)
     {
         if (!IsDamageable())
             return false;
 
+        if (!_isSuperArmor)
+        {
+            OnKnockBack?.Invoke();
+        }
+
+        SetInvincible(_invincibleTimeOnHit);
         _prevHitTime = Time.time;
         damage = Mathf.Max(0f, damage);
         _hp -= damage;
@@ -109,4 +181,53 @@ public class Combat
     {
         Heal(9999999999f);
     }
+
+    int _defaultLayer;
+    public void SetEvade(float time)
+    {
+        OnEvade?.Invoke();
+
+        if(_invincibleTimer < time)
+        {
+            _evadeTimer = time;
+            _owner.layer = LayerMask.GetMask("Evade");
+        }
+    }
+    public void ResetEvade()
+    {
+        _evadeTimer = 0f;
+        _owner.layer = _defaultLayer;
+        OnReleaseEvade?.Invoke();
+    }
+
+    public void SetSuperArmor(float time)
+    {
+        OnInvincible?.Invoke();
+
+        if(_superArmorTimer < time)
+        {
+            _superArmorTimer = time;
+        }
+    }
+    public void ResetSuperArmor()
+    {
+        _superArmorTimer = 0f;
+        OnReleaseSuperArmor?.Invoke();
+    }
+
+    public void SetInvincible(float time)
+    {
+        OnSuperArmor?.Invoke();
+
+        if(_invincibleTimer < time)
+        {
+            _invincibleTimer = time;
+        }
+    }
+    public void ResetInvincible()
+    {
+        _invincibleTimer = 0f;
+        OnReleaseInvincible?.Invoke();
+    }
+
 }
