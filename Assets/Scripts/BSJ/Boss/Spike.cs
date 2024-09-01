a139ba8e10aa4e2c27b77376da604957e89fa063
변석진 <bsj2234@naver.com>
2024-09-02 08:31:22 +0900

    [feat] 보스용 가시 추가

---
+++ Assets/Scripts/BSJ/Boss/Spike.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour, ITargetable
+{
+    [SerializeField] private float _cooldown;
+    [SerializeField] private float _damage;
+    [SerializeField] private float _hp;
+    [SerializeField] private GameObject _trashPrefab;
+    private float _damageTimeStamp;
+
+    public Vector3 GetPosition()
+    {
+        return transform.position;
+    }
+
+    public void Hit(float dmg, DamageType type = DamageType.Normal)
+    {
+        _hp -= dmg;
+        if (_hp <= 0f)
+        {
+            SpawnTrash();
+        }
+    }
+
+    public bool IsDead()
+    {
+        return _hp <= 0f;
+    }
+
+    private void SpawnTrash()
+    {
+        Instantiate(_trashPrefab, transform.position, transform.rotation);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rbOther = collision.rigidbody;
+        if (rbOther == null)
+        {
+            return;
+        }
+
+        if(rbOther.CompareTag("Player"))
+        {
+            if (Time.time >= _damageTimeStamp + _cooldown)
+            {
+                rbOther.GetComponent<PlayerMaster>().Hit(_damage, DamageType.NonKnockback);
+            }
+        }
+    }
+}
