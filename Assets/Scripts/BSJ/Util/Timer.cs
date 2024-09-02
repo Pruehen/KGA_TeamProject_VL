1ec31a5dec1fc8ae37f562677282ffd5e8b2129b
변석진 <bsj2234@naver.com>
2024-09-02 12:08:54 +0900

    [feat] 타이머 클래스 추가

---
+++ Assets/Scripts/BSJ/Util/Timer.cs
@@ -0,0 +1,37 @@
+using System;
+
+public class Timer
+{
+    public Action OnEnd;
+    private float _time;
+    private float _limit;
+    private bool _isStop;
+    public bool IsStop { get { return _isStop; } private set { _isStop = value; } }
+
+
+    public void Init(float time, Action endCallback)
+    {
+        _limit = time;
+        OnEnd += endCallback;
+    }
+
+    public void DoUpdate(float deltaTime)
+    {
+        if (_isStop)
+            return;
+
+        _time += deltaTime;
+
+        if (_time > _limit)
+        {
+            _isStop = true;
+            OnEnd?.Invoke();
+        }
+    }
+
+    public void StartTimer()
+    {
+        IsStop = false;
+        _time = 0f;
+    }
+}
