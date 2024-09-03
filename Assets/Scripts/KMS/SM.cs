734eae540cb5d16860b14430db0577dfe7ecee70
김민석 <ogu590626@gmail.com>
2024-09-03 16:41:16 +0900

    [update] 사운드매니저 수정

--- Assets/Scripts/KMS/SM.cs
+++ Assets/Scripts/KMS/SM.cs
@@ -2,34 +2,69 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class SM : SceneSingleton<SM>
+public class SM : GlobalSingleton<SM>
 {
-    private AudioSource _audioSource;
 
-    private GameObject _soundPrefeb;
-    public SFX Absorbeing;
+    public SFX sfxData; // Assign in the inspector
 
+    private Dictionary<string, AudioClip> audioClipDictionary;
 
-    void Awake()
+    private void Awake()
     {
-        _audioSource = gameObject.AddComponent<AudioSource>();
+        InitializeAudioDictionary();
     }
 
-    public void PlaySound(AudioClip _soundPrefeb,Vector3 _pos)
+    private void InitializeAudioDictionary()
     {
-        
+        audioClipDictionary = new Dictionary<string, AudioClip>
+        {
+            { "playerstep", sfxData.playerstep },
+            { "playerHit", sfxData.playerHit },
+            { "playerDead", sfxData.playerDead },
+            { "Absorbeing", sfxData.Absorbeing },
+            { "playerMeleeTransform", sfxData.playerMeleeTransform },
+            { "playerRangeTransform", sfxData.playerRangeTransform },
+            { "playerRangeProjectileHIt", sfxData.playerRangeProjectileHIt },
+            { "playerMeleeAttackHit", sfxData.playerMeleeAttackHit },
+            { "playerCharging", sfxData.playerCharging },
+            { "playerCharged", sfxData.playerCharged },
+            { "playerChargedAttack", sfxData.playerChargedAttack },
+            { "playerDash", sfxData.playerDash },
+            { "RangeSkill1", sfxData.RangeSkill1 },
+            { "RangeSkill2", sfxData.RangeSkill2 },
+            { "RangeSkill3", sfxData.RangeSkill3 },
+            { "RangeSkill4", sfxData.RangeSkill4 },
+            { "MeleeSkill1", sfxData.MeleeSkill1 },
+            { "MeleeSkill2_1", sfxData.MeleeSkill2_1 },
+            { "MeleeSkill2_2", sfxData.MeleeSkill2_2 },
+            { "MeleeSkill3", sfxData.MeleeSkill3 },
+            { "MeleeSkill4_1", sfxData.MeleeSkill4_1 },
+            { "MeleeSkill4_2", sfxData.MeleeSkill4_2 },
+            { "NPCStep", sfxData.NPCStep },
+            { "NPCHit", sfxData.NPCHit },
+            { "NPCAttack", sfxData.NPCAttack },
+            { "NPCDeath", sfxData.NPCDeath }
+        };
     }
 
-    public void PlaySound2(SFX sfx,Vector3 _pos)
-    {
-        AudioClip audioclips = sfx.clip;
-        GameObject SB = ObjectPoolManager.Instance.DequeueObject(audioclips, _pos);
-        SB.transform.position = _pos;
-        SB.GetComponent<AudioSource>().Play();
 
+    public void PlaySound2(string soundName, Vector3 position)
+    {
+        if (audioClipDictionary.TryGetValue(soundName, out AudioClip audioClip))
+        {
+            GameObject sb = ObjectPoolManager.Instance.DequeueObject(audioClip, position);
+            sb.transform.position = position;
+            AudioSource audioSource = sb.GetComponent<AudioSource>();
+            audioSource.clip = audioClip;
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning($"Sound '{soundName}' not found in SFX data.");
+        }
     }
 
-    public void PlaySoundAtPosition(AudioClip clip, Vector3 position)
+        public void PlaySoundAtPosition(AudioClip clip, Vector3 position)
     {
         AudioSource.PlayClipAtPoint(clip, position);
     }
