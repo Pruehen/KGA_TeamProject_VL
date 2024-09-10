33ca628a79601d1b7ac1ec7cb10b09434fbed745
김민석 <ogu590626@gmail.com>
2024-09-10 10:52:29 +0900

    [update] 상자깡 사운드 연결

--- Assets/Scripts/CJH/Chest.cs
+++ Assets/Scripts/CJH/Chest.cs
@@ -31,6 +31,7 @@ public class Chest : MonoBehaviour
 
     private void SpawnItem(RewardType rewardType)
     {
+        SM.Instance.PlaySound2("boxOpen", transform.position);
         switch (rewardType)
         {
             case RewardType.Currency:
@@ -54,7 +55,6 @@ public class Chest : MonoBehaviour
     private void SpawnRandomGold(int v1, int v2)
     {
         animator.SetTrigger("Chest");
-        SM.Instance.PlaySound2("boxOpen", transform.position);
         chestCollider.enabled = false;
         int amount = Random.Range(v1, v2 + 1);
         GameManager.Instance._PlayerMaster._PlayerInstanteState.AddGold(amount);
