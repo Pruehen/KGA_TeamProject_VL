275eb9a6363cca186bb20a412e2c2dc457c3307b
변석진 <bsj2234@naver.com>
2024-09-11 14:55:05 +0900

    [update] 가시 업데이트 및 플레이어 원거리 가시 데미지 적용

--- Assets/Scripts/BSJ/Boss/Spike.cs
+++ Assets/Scripts/BSJ/Boss/Spike.cs
@@ -11,15 +11,26 @@ public class Spike : MonoBehaviour, ITargetable
     [SerializeField] private GameObject _trashPrefab;
     private float _damageTimeStamp;
 
+    private Collider _collision;
+
     public Action OnDead { get; internal set; }
+
+    private bool _isDestroyable = false;
 
     public Vector3 GetPosition()
     {
         return transform.position;
     }
 
+    private void Awake()
+    {
+        _collision = GetComponent<Collider>();
+    }
+
     public void Hit(float dmg, DamageType type = DamageType.Normal)
     {
+        if (!_isDestroyable)
+            return;
         _hp -= dmg;
         if (_hp <= 0f)
         {
@@ -30,12 +41,12 @@ public class Spike : MonoBehaviour, ITargetable
 
     public bool IsDead()
     {
-        SM.Instance.PlaySound2("boss_SpikeBroken", transform.position);
         return _hp <= 0f;
     }
 
     private void SpawnTrash()
     {
+        SM.Instance.PlaySound2("boss_SpikeBroken", transform.position);
         GameObject spike = ObjectPoolManager.Instance.DequeueObject(_trashPrefab, transform.position);
         spike.transform.rotation = transform.rotation;
     }
@@ -61,5 +72,25 @@ public class Spike : MonoBehaviour, ITargetable
         {
             Hit(999f);
         }
+    }
+
+    public void DisablePhysics()
+    {
+        _collision.enabled = false;
+    }
+    public void EnablePhysics()
+    {
+        _collision.enabled = true;
+        _isDestroyable = false;
+    }
+
+    public void EnableSpike()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void EnableDestroy()
+    {
+        _isDestroyable = true;
     }
 }
--- Assets/Scripts/BSJ/Boss/SpikeSpawner.cs
+++ Assets/Scripts/BSJ/Boss/SpikeSpawner.cs
@@ -8,10 +8,29 @@ public class SpikeSpawner : MonoBehaviour
     [SerializeField] private Timer _spikeTimer;
     [SerializeField] private Spike _spike;
     [SerializeField] private GameObject _anticipateModel;
+    [SerializeField] private GameObject _model;
+    [SerializeField] private GameObject _spawnEffect;
 
+    [SerializeField] private Timer _spikeModelTimer;
+    [SerializeField] private Timer _enqueueTimer;
+
+    ParticleSystem _anticipateParticle;
+    ParticleSystem _modelParticle;
+    ParticleSystem _spawnParticle;
+    private void Awake()
+    {
+        _anticipateParticle = _anticipateModel.GetComponent<ParticleSystem>();
+        _modelParticle = _model.GetComponent<ParticleSystem>();
+        _spawnParticle = _spawnEffect.GetComponent<ParticleSystem>();
+
+        _spikeModelTimer.Init(.3f, PauseModel);
+        _enqueueTimer.Init(3f, Enqueue);
+    }
     private void OnEnable()
     {
         _anticipateModel.SetActive(true);
+        _anticipateParticle.Play();
+
         _spike.gameObject.SetActive(false);
         _spikeTimer.StartTimer();
         _spikeTimer.OnEnd += EnableSpike;
@@ -21,20 +40,34 @@ public class SpikeSpawner : MonoBehaviour
     private void Update()
     {
         _spikeTimer.DoUpdate(Time.deltaTime);
+        _spikeModelTimer.DoUpdate(Time.deltaTime);
+        _enqueueTimer.DoUpdate(Time.deltaTime);
     }
 
     private void EnableSpike()
     {
-        _spike.gameObject.SetActive(true);
+        _spike.EnableSpike();
+        _modelParticle.Play();
+        _spikeModelTimer.StartTimer();
+        _spike.EnablePhysics();
+        _spawnParticle.Play();
+
         _anticipateModel.SetActive(false);
-    }
-    private void DisableSelf()
-    {
-        gameObject.SetActive(false);
     }
     private void DoReset()
     {
+        _modelParticle.Play();
+        _spike.DisablePhysics();
         _spikeTimer.ResetTimer();
+    }
+    private void PauseModel()
+    {
+        _spike.EnableDestroy();
+        _modelParticle.Pause();
+    }
+    private void Enqueue()
+    {
+        _spawnParticle.Clear();
         ObjectPoolManager.Instance.EnqueueObject(gameObject);
     }
 }
