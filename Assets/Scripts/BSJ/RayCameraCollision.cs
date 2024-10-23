8fdf765dc0aaed5ac117fc022e18faf726552856
변석진 <bsj2234@naver.com>
2024-10-23 23:19:41 +0900

    [update] 카메라 충돌 로직 수정 완료

--- Assets/Scripts/BSJ/RayCameraCollision.cs
+++ Assets/Scripts/BSJ/RayCameraCollision.cs
@@ -7,17 +7,23 @@ public class RayCameraCollision : MonoBehaviour
     [SerializeField] Transform _cameraParent;
     [SerializeField] Transform _camera;
     [SerializeField] LayerMask _collisionLayers;
-    [SerializeField] float _cameraDistance = 3f;
+    [SerializeField] Transform _originalCameraTarget;
     [SerializeField] float _offsetFromWall = 0.3f;
     [SerializeField] Transform _repositionCameraTarget;
 
     void Update()
     {
+        float _cameraDistance = Vector3.Distance(_cameraParent.position, _originalCameraTarget.position);
+        Debug.DrawRay(_cameraParent.position, -_camera.forward * _cameraDistance, Color.red);
         if(Physics.Raycast(_cameraParent.position, -_camera.forward, out RaycastHit hit, _cameraDistance, _collisionLayers))
         {
-            _repositionCameraTarget.position = _cameraParent.position - (_cameraParent.forward * (hit.distance - _offsetFromWall));
-            Debug.Log("Hit");
-        }
+            _repositionCameraTarget.position =
+             -_camera.forward * (hit.distance - _offsetFromWall) + _cameraParent.position;
+            Debug.DrawRay(_repositionCameraTarget.position, Vector3.forward, Color.green);
+            Debug.Log($"Hit1 : {_camera.forward * (hit.distance - _offsetFromWall)}");
+            Debug.Log($"Hit2 : {_repositionCameraTarget.position}");
+            Debug.Log($"Hit3 : {_repositionCameraTarget.localPosition}");
+        } 
         else
         {
             _repositionCameraTarget.localPosition = Vector3.zero;
